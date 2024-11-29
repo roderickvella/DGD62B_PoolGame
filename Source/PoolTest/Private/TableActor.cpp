7d945bfa9f04077fcd9fee8b07fc8d0b9660cb80
roderick <roderick.vella@mcast.edu.mt>
2024-11-29 13:51:42 +0100

    Lesson2

--- Source/PoolTest/Private/TableActor.cpp
+++ Source/PoolTest/Private/TableActor.cpp
@@ -9,6 +9,12 @@ ATableActor::ATableActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	RootComponent = DefaultSceneRoot;
+
+	//set the default scale for the root component
+	DefaultSceneRoot->SetRelativeScale3D(FVector(2.5f, 2.5f, 2.5f));
+
 }
 
 // Called when the game starts or when spawned
